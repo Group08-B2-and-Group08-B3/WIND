80a807867b53410833925bfaeac88d03334a5699
cyriljchan <42668375+cyriljchan@users.noreply.github.com>
2020-04-28 18:39:15 +0800

    Changed the code
    instead of bs4, the url was made into a json format so that the text will be easier to handle and added into the dataset

--- Project W.I.N.D WebScrapping (from OpenWeather).py
+++ Project W.I.N.D WebScrapping (from OpenWeather).py
@@ -1,23 +1,36 @@
-#import libs
-from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen
+#import the necessary libs
+import pandas as pd
+import datetime
+from csv import writer
 
-#url of the OpenWeather API
-url = "https://api.openweathermap.org/data/2.5/onecall?lat=14.590456&lon=120.9774225&units=metric&current.dt&appid={API Key}"
+#to append data to the next row of the existing dataset(.csv)
+def appendrow(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
 
-#Website Actions
-url = urlopen(url)
-urlRead = url.read()
-url.close()
+#takes the url
+url = "https://api.openweathermap.org/data/2.5/onecall?lat=14.590456&lon=120.9774225&units=metric&current.dt&appid=40b08dff1c7e4fc895c7a3d3d9978d92"
+jsonData = requests.get(url).json()
 
-#file open for insertion of data
-file = 'dataset.txt'
-f = open(file, 'w')
+# get from dataframe and jsondata
+lat,lon = jsonData['lat'],jsonData['lon']
+tz = jsonData['timezone']
+df = jsonData['current']
+dt = df['dt'] 
+temp = df['temp']
+feels = df['feels_like'] 
+pres = df['pressure'] 
+hum = df['humidity']
 
-#writes the headers for the dataset
-headers = "date/time, temp, feels_like, humidity, wind_speed\n"
-f.write(headers)
+#converts dt(a unix timestamp format) into GMT+8
+dt = datetime.datetime.fromtimestamp(int(dt)).strftime('%Y-%m-%d %H:%M:%S')
+print(dt)
 
-#BS Actions
-#parses html into a soup data structure to traverse html.
-html = bs(urlRead, "html.parser")
+#place data into a list
+weatherdata = [lat,lon,tz,dt,temp,feels,pres,hum]
+#append list into dataset
+appendrow('weather.csv', weatherdata)
