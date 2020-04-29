5ba768b5df03e325e5333a5be9f24e9e4a4c9f34
cyriljchan <42668375+cyriljchan@users.noreply.github.com>
2020-04-29 16:20:58 +0800

    Edits on the collected data
    - removed feels_like
    - removed wind_deg

--- Project W.I.N.D WebScrapping (from OpenWeather).py
+++ Project W.I.N.D WebScrapping (from OpenWeather).py
@@ -22,17 +22,15 @@ tz = jsonData['timezone']
 df = jsonData['current']
 dt = df['dt'] 
 temp = df['temp']
-feels = df['feels_like'] 
 pres = df['pressure'] 
 hum = df['humidity']
 wspeed = df['wind_speed']
-wdeg = df['wind_deg']
 
 #converts dt(a unix timestamp format) into GMT+8
 dt = datetime.datetime.fromtimestamp(int(dt)).strftime('%Y-%m-%d %H:%M:%S')
 print(dt)
 
 #place data into a list
-weatherdata = [lat,lon,tz,dt,temp,feels,pres,hum,wspeed,wdeg]
+weatherdata = [lat,lon,tz,dt,temp,pres,hum,wspeed]
 #append list into dataset
 appendrow('weather.csv', weatherdata)
